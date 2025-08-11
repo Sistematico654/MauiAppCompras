725a256b973f9207314d1031822dc92e0737a77a
João <jhsp350@gmail.com>
2025-08-11 10:48:49 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppCompras/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
